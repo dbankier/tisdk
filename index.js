ede8e991c69e970b815028e92f90bb5d7942dbaa
David Bankier <dbankier@hotmail.com>
2015-07-06 18:17:20 +1000

    and the code

---
+++ index.js
@@ -0,0 +1,25 @@
+var request = require("request");
+
+exports.getGATags = function getGATags(callback) {
+  request({
+    url: "https://api.github.com/repos/appcelerator/titanium_mobile/tags",
+    headers: {
+      'User-Agent': 'Awesome-Octocat-App'
+    }
+  }, function(err, res) {
+    var tags = JSON.parse(res.body).filter(function(a) { return a.name.match(/^\d+_\d+_\d+_.*/); });
+    callback(tags);
+  });
+}
+
+exports.getNightlies = function getNightlies(branch, callback) {
+  request({
+    url: "http://builds.appcelerator.com/mobile/" + branch + "/index.json",
+    headers: {
+//      'User-Agent': 'Awesome-Octocat-App'
+    }
+  }, function(err, res) {
+    callback(JSON.parse(res.body));
+  });
+};
+
